eaa4e7b8965ef2a81655c9e94d643a615c243d4e
Andre de Souza <andre@MacBook-Pro-de-Andre.local>
2018-09-07 10:00:03 -0300

    Um pequena melhoria nos testes e alteração no nome do arquivo que estava errado

--- recipies/recipies.test.ts
+++ recipies/recipies.test.ts
@@ -3,10 +3,15 @@ import * as request from 'supertest'
 
 
 test('GET /recipies with three parameters', ()=>{
+  const keywords = ['tomato','onion','garlic']
   return request('localhost:3030')
-    .get('/recipies?i=tomato,onion,garlic')
+    .get(`/recipies?i=${keywords.join()}`)
     .then(response=>{
       expect(response.status).toBe(200)
+      expect(response.body).toBeDefined()
+      expect(response.body.keywords).toEqual(keywords)
+      expect(response.body.recipes).toBeInstanceOf(Array)
+      expect(response.body.recipes.length).toBeGreaterThan(0)
     })
 })
 
--- services/services.test.ts
+++ services/services.test.ts
@@ -5,6 +5,8 @@ test('GET GIPHY API', ()=>{
   return services.giphyRequest('Simple Jalapeno Salsa')
                   .then(response=>{
                     expect(response).toBeDefined()
+                    expect(response.data).toBeInstanceOf(Array)
+                    expect(response.data.length).toEqual(1)
                   })
 })
 
@@ -12,5 +14,7 @@ test('GET Recipe Puppy API', ()=>{
   return services.recipePuppyRequest('onion,tomato,garlic')
                   .then(response=>{
                     expect(response).toBeDefined()
+                    expect(response.results).toBeInstanceOf(Array)
+                    expect(response.results.length).toBeGreaterThan(0)
                   })
 })
