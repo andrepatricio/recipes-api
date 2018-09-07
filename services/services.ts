228831eb533b6ac28be1a45260db168201cd2a14
Andre de Souza <andre@MacBook-Pro-de-Andre.local>
2018-09-07 10:02:11 -0300

    Arrumando o retorno da Promise

--- services/services.ts
+++ services/services.ts
@@ -2,30 +2,23 @@ import * as rp from 'request-promise'
 import {environment} from '../commons/environment'
 
 export class Services{
-  recipePuppyRequest(ingredients:string): Promise<any>{
-    return new Promise<Services>((resolve, reject)=>{
-      rp({
-          uri: `${environment.services.recipePuppy.url}${ingredients}`,
-          json: true
-        })
-        .then(resolve)
-        .catch(reject)
-    })
+  recipePuppyRequest(ingredients:string): rp.RequestPromise{
+    return rp({
+            uri: `${environment.services.recipePuppy.url}${ingredients}`,
+            json: true
+          })
   }
 
-  giphyRequest(query: string): Promise<any>{
-    return new Promise<Services>((resolve, reject)=>{
-      rp({
-          uri: `${environment.services.giphy.url}`,
-          qs: {
-            key: environment.services.giphy.apiKey,
-            q: query,
-            limit: 1
-          }
-        })
-        .then(resolve)
-        .catch(reject)
-    })
+  giphyRequest(query: string): rp.RequestPromise{
+    return rp({
+              uri: `${environment.services.giphy.url}`,
+              qs: {
+                key: environment.services.giphy.apiKey,
+                q: query,
+                limit: 1
+              },
+              json: true
+            })
   }
 
 }
